8167aff5e78aade649fb10196fdd1d2d4604ab91
galadd <gbolahanf.14@gmail.com>
2023-10-27 00:16:32 +0100

    Wrapper for nimterop C

---
+++ sqlite3_abi/wrap_wal.nim
@@ -0,0 +1,38 @@
+import nimterop/cimport
+
+static:
+  # uses va_list which is undefined
+  cSkipSymbol(@[
+    # uses va_list which is undefined
+    "sqlite3_vmprintf",
+    "sqlite3_vsnprintf",
+    "sqlite3_str_vappendf",
+    # can use native nim types instead
+    "sqlite_int64",
+    "sqlite_uint64",
+    "sqlite3_int64",
+    "sqlite3_uint64",
+    # Code reording problems
+    "SQLITE_STATIC",
+    # cchar problem
+    "sqlite3_version"
+    ])
+
+  cOverride:
+    type sqlite3_index_info* {.importc, bycopy, incompleteStruct.} = object
+
+cPlugin:
+  import strutils
+
+  proc onSymbol*(sym: var Symbol) {.exportc, dynlib.} =
+    if sym.kind == nskType:
+      case sym.name
+      of "sqlite_int64", "sqlite3_int64": sym.name = "int64"
+      of "sqlite_uint64", "sqlite3_uint64": sym.name = "uint64"
+
+cCompile("sqlite3.c")
+
+cImport("wal.h", flags="-H", nimfile="wal_gen.nim")
+
+
+{.passL: "-lpthread".}
